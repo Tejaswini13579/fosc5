70f5361ee4b842af8d628910dbf2b3da23435317
nitesh kudmethe <niteshkudmethe@niteshs-MacBook-Pro.local>
2023-06-06 18:55:30 +0530

    Initial code for FOS frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { User } from './User';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  userId: string | null = null; // Declare userId property with initial value null
+  private userIdSubject: Subject<string | null> = new Subject<string | null>();
+
+  private _isLoggedIn: boolean = false; // private backing field for isLoggedIn
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.userId = this.authService.getUserId(); // Retrieve the userId from the service
+    }
+
+    this.authService.subscribeToUserIdChanges().subscribe(
+      (userId: string | null) => {
+        this.userId = userId;
+      },
+      (error: any) => {
+        console.error('Error fetching user ID:', error);
+      }
+    );
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
